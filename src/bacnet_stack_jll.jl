67525644b378848aec8ee2c91a43ccbd18e3d35e
jlbuild <juliabuildbot@gmail.com>
2026-06-16 23:20:45 +0000

    bacnet_stack_jll build 1.4.1+1

--- src/bacnet_stack_jll.jl
+++ src/bacnet_stack_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("bacnet_stack")
-JLLWrappers.@generate_main_file("bacnet_stack", UUID("45b21b7f-0fc3-514f-951b-76478cbee2c7"))
+JLLWrappers.@generate_main_file("bacnet_stack", Base.UUID("45b21b7f-0fc3-514f-951b-76478cbee2c7"))
 end  # module bacnet_stack_jll
